34366f921717bea36291abffc346341346c928fb
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 20:14:31 +0300

    Create IUrlService interface

---
+++ backend/URLShortener.Core.BLL/Interfaces/IUrlService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URLShortener.Core.DAL.Entitites;
+
+namespace URLShortener.Core.BLL.Interfaces
+{
+    public interface IUrlService
+    {
+        Task<Url> GetUrl(uint id);
+
+        Task<ICollection<Url>> GetUrls();
+
+        Task<string> ShortenUrl(string url);
+    }
+}
