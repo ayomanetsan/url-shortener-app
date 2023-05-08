f647b0834edc59ea5f7a7a69472bd613ce1476d0
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 15:09:25 +0300

    Implement sign up client method

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +9,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  
+  constructor(private http: HttpClient) {}
+  
   regForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
@@ -15,6 +20,17 @@ export class RegisterComponent {
   });
 
   signUp() {
-    
+    const formData = this.regForm.value;
+
+    console.log(formData);
+
+    this.http.post(`${environment.apiBaseUrl}/auth/register`, formData).subscribe(
+      (response) => {
+        console.log('API call successful:', response);
+      },
+      (error) => {
+        console.log('API call error:', error);
+      }
+    );
   }
 }
