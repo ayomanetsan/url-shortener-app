b6c8cff5a8aa4e390c1f1378be5f9f7237e335eb
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 20:12:14 +0300

    Create Url entity

--- backend/URLShortener.Core.DAL/Context/UrlShortenerDbContext.cs
+++ backend/URLShortener.Core.DAL/Context/UrlShortenerDbContext.cs
@@ -11,5 +11,6 @@ namespace URLShortener.Core.DAL.Context
         }
 
         public DbSet<User> Users { get; set; }
-    }
+        public DbSet<Url> Urls { get; set; }
+     }
 }
---
+++ backend/URLShortener.Core.DAL/Entitites/Url.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Core.DAL.Entitites
+{
+    public class Url
+    {
+        public uint Id { get; set; }
+        public string ShortUrl { get; set; } = string.Empty;
+        public string OriginalUrl { get; set; } = string.Empty;
+        public User CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
