a80b9bde810b6f873b273fd1d458eaa22d0dd44c
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 13:22:54 +0300

    Create auth controller

--- backend/URLShortener.Core/Controllers/AuthController.cs
+++ backend/URLShortener.Core/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using URLShortener.Core.BLL.Interfaces;
+using URLShortener.Core.Common.DTO;
+using URLShortener.Core.DAL.Entitites;
 
 namespace URLShortener.Core.Controllers
 {
@@ -12,6 +14,34 @@ namespace URLShortener.Core.Controllers
         public AuthController(IAuthService authService)
         {
             _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto user)
+        {
+            try
+            {
+                var newUser = await _authService.LoginAsync(user);
+                return CreatedAtAction(nameof(Register), newUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(User user)
+        {
+            try
+            {
+                var newUser = await _authService.RegisterAsync(user);
+                return CreatedAtAction(nameof(Register), newUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
