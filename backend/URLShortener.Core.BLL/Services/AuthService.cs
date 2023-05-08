ad53c74eba7a5112d69b2a0146449d81d14fc9b7
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 12:36:42 +0300

    Implement IAuthService interface

--- backend/URLShortener.Core.BLL/Services/AuthService.cs
+++ backend/URLShortener.Core.BLL/Services/AuthService.cs
@@ -1,13 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using URLShortener.Core.BLL.Interfaces;
+﻿using URLShortener.Core.BLL.Interfaces;
 using URLShortener.Core.Common.DTO;
 using URLShortener.Core.DAL.Context;
 using URLShortener.Core.DAL.Entitites;
@@ -30,44 +21,12 @@ namespace URLShortener.Core.BLL.Services
 
         public async Task<UserDto> RegisterAsync(User user)
         {
-            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
-
-            if (dbUser == null)
-            {
-                _context.Users.Add(user);
-                await _context.SaveChangesAsync();
-
-                var token = GenerateAccessToken(user);
-
-                return new UserDto
-                {
-                    FullName = user.FirstName + " " + user.LastName,
-                    IsAdmin = false,
-                    Token = token,
-                };
-            }
-            else
-            {
-                throw new Exception("User already exists. Please try logging in instead.");
-            }
+            throw new NotImplementedException();        
         }
 
         public string GenerateAccessToken(User user)
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes("H@McQfTjWnZr4u7w");
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.Email),
-                    new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User")
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+            throw new NotImplementedException();
         }
     }
 }
---
+++ backend/URLShortener.Core.Common/DTO/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Core.Common.DTO
+{
+    public class UserDto
+    {
+        public string FullName {  get; set; } = string.Empty;
+        public bool IsAdmin { get; set; }
+        public string Token { get; set; } = string.Empty;
+    }
+}
