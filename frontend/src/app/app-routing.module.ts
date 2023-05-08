d9f702f710b682605655bff78365fbb04a3fcb72
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-08 13:33:41 +0300

    Create sign up page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'sign-up', component: RegisterComponent },
+  ]),
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
