010814d3169bd94d6a17131a4aa16017d5c42502
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-09 19:55:03 +0300

    Update url creation modal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { CreationModalComponent } from './components/creation-modal/creation-mod
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
--- frontend/src/app/components/creation-modal/creation-modal.component.ts
+++ frontend/src/app/components/creation-modal/creation-modal.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { catchError, of } from 'rxjs';
+import { UrlService } from 'src/app/core/services/url.service';
 
 @Component({
   selector: 'app-creation-modal',
   templateUrl: './creation-modal.component.html',
   styleUrls: ['./creation-modal.component.css']
 })
-export class CreationModalComponent {
+export class CreationModalComponent implements OnInit {
 
+  urlForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialogRef: MatDialogRef<CreationModalComponent>,
+    private url: UrlService
+  ) { }
+
+  ngOnInit() {
+    this.urlForm = this.formBuilder.group({
+      ['url']: ['', [Validators.required, Validators.pattern('https?://.+')]]
+    });
+  }
+
+  createUrl() {
+    this.url.Shorten(this.urlForm.value).pipe(
+      catchError(() => {
+        alert('This URL already exists!');
+        return of(null);
+      })
+    ).subscribe(() => {
+        this.dialogRef.close();
+    });
+  }
 }
