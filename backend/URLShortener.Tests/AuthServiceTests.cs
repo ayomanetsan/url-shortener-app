4ccefad28284c50443947e59f3bd00c7a9f27df4
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-05-10 07:17:43 +0300

    Create login test

--- backend/URLShortener.Tests/AuthServiceTests.cs
+++ backend/URLShortener.Tests/AuthServiceTests.cs
@@ -2,7 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using URLShortener.Core.BLL.Interfaces;
 using URLShortener.Core.BLL.Services;
+using URLShortener.Core.Common.DTO;
 using URLShortener.Core.DAL.Context;
+using URLShortener.Core.DAL.Entitites;
 
 namespace URLShortener.Tests
 {
@@ -28,6 +30,31 @@ namespace URLShortener.Tests
                 .Build();
 
             _sut = new AuthService(new UrlShortenerDbContext(_options), _configuration);
+        }
+
+        [Fact]
+        public async Task LoginAsyncWithValidCredentialsReturnsUserDto()
+        {
+            var user = new User { 
+                FirstName = "Test", 
+                LastName = "User", 
+                Email = "testuser@test.com", 
+                Password = "password" 
+            };
+
+            using (var context = new UrlShortenerDbContext(_options))
+            {
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+            }
+
+            var loginDto = new LoginDto { Email = "testuser@test.com", Password = "password" };
+            var expected = await _sut.LoginAsync(loginDto);
+
+            Assert.NotNull(expected);
+            Assert.Equal("Test User", expected.FullName);
+            Assert.False(expected.IsAdmin);
+            Assert.NotNull(expected.Token);
         }
     }
 }
